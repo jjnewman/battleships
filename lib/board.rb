cb704d5cd75ebc6b108f1a3ff624c24f0567e1a9
Jacob Mitchinson <jmitchinson+github@gmail.com>
2014-12-16 19:56:13 +0000

    added ability to add ships

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,31 @@
 class Board
 
+  attr_reader :cells
+
+  def initialize
+
+    @cells = [] 
+
+  end
+
+  def add_ship(ship, cell_position)
+
+    # we have a cell in the board
+    # ship needs to be stored in a cell 
+    # we need to read cells
+    # and place where requested
+    # we need to add that cell to the cells
+
+    requested_cell = find_cell(cell_position)
+    requested_cell.sample.add_ship(ship)
+  end
+
+  def find_cell(cell_position)
+    @cells.select do |cell|
+      cell.name == cell_position
+    end
+  end 
+
+  
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,21 +1,20 @@
 require 'board'
+require 'cell'
 
 describe Board do  
 
+  # the problem is that the cell is a double and doesn't know what contains is
+
   let(:ship) { double :ship, size: 1 }
-  let(:cell) { double :cell, name: 'a1', contains: ship }
+  let(:cell) { Cell.new }
   let(:board) { Board.new }
 
   it 'can place ship' do
     # add ship to cell
     # add cell to board
-    board.add_ship
-    expect(cell.contains).to eq(ship)
+    board.add_ship(ship, 'a1')
+    expect(board.find_cell('a1')).to eq(ship)
   end
-
-    # place ship
-    # check ship position
-
 
 
 end
