12742c73fe7f5f54eeb13275899c3b81efae2d82
gabe <GabeMaker@users.noreply.github.com>
2015-02-10 12:37:31 +0000

    ship starts with no hits, set size test

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,19 +1,14 @@
 class Ship
 
+  attr_reader :hit_count
 
+  def initialize
+    @hit_count = 0
+  end
 
-def hit!
-
-
-end
-
-def hit_count
-	1
-
-
-end
-
-
+  def hit!
+    @hit_count += 1
+  end
 
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -8,12 +8,14 @@ let(:ship){Ship.new}
 		expect(ship.hit_count).to eq 0
 	end
 
-
 	it 'should be able to be hit' do
 		ship.hit!
 		expect(ship.hit_count).to eq 1
 	end
 
-
+  it 'should have a size, set on initialising' do
+    new_ship = Ship.new(3)
+    expect(new_ship.size).to eq 3
+  end 
 	
 end
