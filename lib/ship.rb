0679c3610bf285ee8cccc73d726dd84795b368b1
gabe <GabeMaker@users.noreply.github.com>
2015-02-11 17:43:22 +0000

    can make all ship sizes

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,13 +1,36 @@
 class Ship
 
-  attr_reader :hit_count
+  attr_reader :hit_count, :size
 
-  def initialize
+  def initialize(size=1)
     @hit_count = 0
+    @size = size
+  end
+
+  def self.patrol_boat
+    new(2)
+  end
+
+  def self.destroyer
+    new(3)
+  end
+
+  def self.submarine
+    new(3)
+  end
+
+  def self.battleship
+    new(4)
+  end
+
+  def self.aircraft_carrier
+    new(5)
   end
 
   def hit
     @hit_count += 1
   end
+
+
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -10,11 +10,35 @@ describe Ship do
     expect(ship.hit_count).to eq 1
   end
 
+  it "should be size 1 by default" do
+    ship = Ship.new
+    expect(ship.size).to eq 1
+  end
+
   it 'can be created as a patrol boat' do
     patrol_boat = Ship.patrol_boat
     expect(patrol_boat.size).to eq 2
   end
 
+  it 'can be created as a destroyer' do
+    destroyer = Ship.destroyer
+    expect(destroyer.size).to eq 3
+  end
+
+  it 'can be created as a submarine' do
+    submarine = Ship.submarine
+    expect(submarine.size).to eq 3
+  end
+
+  it 'can be created as a battleship' do
+    battleship = Ship.battleship
+    expect(battleship.size).to eq 4
+  end
+
+  it 'can be created as an aircraft carrier' do
+    aircraft_carrier = Ship.aircraft_carrier
+    expect(aircraft_carrier.size).to eq 5
+  end
 
 
   xit "should be able to sink a ship of length one with one hit" do
