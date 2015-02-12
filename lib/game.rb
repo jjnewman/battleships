f762cf3d91ac67b525c0a49685926a094301cb4d
Joe Newman <makerslaptop23@Makerss-MacBook-Pro-6.local>
2015-02-12 20:36:43 +0000

    player turns

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,30 @@
 class Game
 
-attr_reader :player_one , :player_two, :winner
+  attr_reader :player_one , :player_two, :winner
 
-def initialize
-  @winner = nil
+  def initialize
+    @winner = nil
+  end
+
+  def add_players(player_one, player_two)
+    @player_one = player_one
+    @player_two = player_two
+  end
+
+  def turn
+    @turn ||= player_one
+  end
+
+  def end_turn
+    turn
+    if @turn == player_one
+      @turn = player_two
+    elsif @turn == player_two
+      @turn = player_one
+    end
+  end
+
 end
-
-def add_players(player1, player2)
-  @player_one = player1
-  @player_two = player2
-end
-
 # def winner? player
 #   player.opponents_board.all_sunk?
 # end
@@ -24,5 +38,3 @@ end
 #     if winner? player2
 #     @winner = player2
 #     end
-
-end
